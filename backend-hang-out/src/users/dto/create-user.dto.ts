89e27b3b9d1a56ce8b3c5d1bbab2caa6ddb81578
Antoine <antoine.roy@tetras-libre.fr>
2024-04-17 13:52:24 +0200

    create user service implementation

--- backend-hang-out/src/users/dto/create-user.dto.ts
+++ backend-hang-out/src/users/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+
+    name: string;
+    mail: string;
+    firstName: string;
+    password: string;
+    createdAt?: Date;
+}
--- backend-hang-out/src/users/users.service.ts
+++ backend-hang-out/src/users/users.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {CreateUserDto} from './dto/create-user.dto';
+import {UpdateUserDto} from './dto/update-user.dto';
+import {InjectRepository} from "@nestjs/typeorm";
+import {DeleteResult, Repository} from "typeorm";
+import {User} from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectRepository(User) private data: Repository<User>) {}
+  create(dto: CreateUserDto): Promise<User> {
+    return this.data.save(dto);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(): Promise<User[]> {
+    return this.data.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number): Promise<User | void> {
+    try {
+      return await this.data.findOneBy({id});
+    } catch (err) {
+      throw new NotFoundException(err);
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, dto: UpdateUserDto): Promise<User | void> {
+    try{
+      let done = await this.data.update(id, dto);
+    if(done.affected != 1){
+      throw new NotFoundException(id);
+    }
+    return await this.findOne(id);
+    }catch(error){
+      throw new error;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number): Promise<void> {
+    let done: DeleteResult = await this.data.delete(id);
+    if(done.affected != 1){
+      throw new NotFoundException(id);
+    }
   }
 }
