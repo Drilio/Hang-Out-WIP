c86ac759286cb87b31c865e8e7112b0320f7524b
Antoine <antoine.roy@tetras-libre.fr>
2024-04-17 19:19:49 +0200

    Fix bug on user mail definition and default createdAt value

--- backend-hang-out/src/app.module.ts
+++ backend-hang-out/src/app.module.ts
@@ -13,9 +13,9 @@ import {ConfigModule} from "@nestjs/config";
         }
     ),
     TypeOrmModule.forRoot( {
-      type: 'mysql',
+      type: 'mariadb',
       host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
+      port: parseInt(process.env.DB_PORT,10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
